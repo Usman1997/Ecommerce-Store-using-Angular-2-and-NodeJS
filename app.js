eb3536a33846f1df259fc9084b62dc57d3828058
Usman Siddiqui <usmaan.siddiquii1@gmail.com>
2018-02-04 16:47:49 +0500

    Backend for user completed. Make a seperate repo later for user authentication backend ready

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use(bodyParser.json());
 
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
 app.use('/users',users);
 app.get('/',(req,res,send)=>{
  res.send('Invalid Enpoints');
